c4b3b1cfd59a1f78bc61724bb1a355df56ebb06b
Hacker <minubgp@gmail.com>
2026-09-22 20:45:44 +0530

    Add main function to print variables and their sum

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+func main(){
+  fmt.Println("Hello")
+  var x int = 10
+  var y int = 50
+  fmt.Println("x is", x)
+  fmt.Println("y is", y)
+  fmt.Println("Sum of x and y is", x+y)
+}
